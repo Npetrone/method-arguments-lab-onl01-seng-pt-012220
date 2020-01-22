8e0bd2c54822272a3f746e145c4b79f6ebc1510e
Nicholas Petrone <nickpetrone92@gmail.com>
2020-01-22 18:49:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-introduction("Nick")
-
 
 def introduction(name, language)
   puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end
-introduction("Nick","Ruby")
